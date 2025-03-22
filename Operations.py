a86cc69350a821cbe2c5f5f10ed03cc6e7aa3d4f
jimboweb <jimboweb@gmail.com>
2025-03-22 11:14:10 -0400

    wrote addition function, I think it's right

--- Operations.py
+++ Operations.py
@@ -1,8 +1,23 @@
-def add_points(x1,x2,y1,y2):
-    if x1 == x2 and y1 == y2:
-        rise = y2 - y1
-        run = x2 - x1
-        slope = rise/run
+from Point import Point
+def add_points(p1, p2, A = None):
+    if p1.infinity:
+        return p2
+    if p2.infinity:
+        return p1
+    x_sum = None
+    y_sum = None
+    if p1.x == p2.x and p1.y == p2.y:
+        rise = p1.y - p2.y
+        run = p1.x - p2.x
+        m = rise/run
+        x_sum = m - p1.x - p2.x
+        y_sum = m * (2*p1.x - m**2+p2.x) - p1.y
 
     else:
-        pass
+        if not A:
+            raise ValueError("if p1 and p2 are equal, you must give an argument for A")
+        dydx = (3*p1.x**2 + A)
+        x_sum = dydx**2/2*p1.y - 2*p1.x
+        y_sum = dydx*(3*p1.x-dydx**2)-p1.y
+    return Point(x_sum, y_sum)
+
--- Point.py
+++ Point.py
@@ -1,6 +1,10 @@
 class Point:
-    def __init__(self, x1, x2, y1, y2):
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
+    #need a way to represent infinity.
+    def __init__(self, x,y, infinity = False):
+        self.infinity = infinity
+        if infinity:
+            self.x = None
+            self.y = None
+        else:
+            self.x = x
+            self.y = y
